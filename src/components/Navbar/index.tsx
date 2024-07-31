b3a1a0422a1f0b38c7ed91e903bc492cb3a5ee94
daviddiazh <daviddiazlds1221@gmail.com>
2024-07-30 23:55:58 -0500

    feat: Navbar component ok

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,59 +11,66 @@ export const Navbar = () => {
     return (
         <div>
             <div className={styles.desktop}>
-                <div className={styles.container}>
-                    <Link to='/'>
-                        <p>LOGO</p>
-                    </Link>
+                <div className={styles.width}>
+                    <div className={styles.container}>
+                        <Link to='/'>
+                            <p>LOGO</p>
+                        </Link>
 
-                    <div className={styles['child-container']}>
-                        <Link to='/payments' className={styles.item}>
-                            <p className={styles.text}>Pagos</p>
-                        </Link>
-                        <Link to='/contact' className={styles.item}>
-                            <p className={styles.text}>Contacto</p>
-                        </Link>
-                        <Link to='/about' className={styles.item}>
-                            <p className={styles.text}>Sobre Nosotros</p>
-                        </Link>
-                    </div>   
+                        <div className={styles['child-container']}>
+                            <Link to='/payments' className={styles.item}>
+                                <p className={styles.text}>Pagos</p>
+                            </Link>
+                            <Link to='/contact' className={styles.item}>
+                                <p className={styles.text}>Contacto</p>
+                            </Link>
+                            <Link to='/about' className={styles.item}>
+                                <p className={styles.text}>Sobre Nosotros</p>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div className={styles.mobile}>
-                <div className={styles.container}>
-                    <Link to='/'>
-                        <p>LOGO</p>
-                    </Link>
+                <div className={styles.width}>
+                    <div className={styles.container}>
+                        <Link to='/'>
+                            <p>LOGO</p>
+                        </Link>
 
-                    {
-                        isOpen ? (
-                            <Drawer
-                                anchor='right'
-                                open={true}
-                                onClose={() => setIsOpen(!isOpen)}
-                            >
-                                <div onClick={() => setIsOpen(!isOpen)} className={styles['btn-close']}>
-                                    <Icon name='x' />
-                                 </div>
-                                <div className={styles['child-container-mobile']}>
-                                     <Link to='/payments' className={styles.item}>
-                                         <p className={styles.text}>Pagos</p>
-                                     </Link>
-                                     <Link to='/contact' className={styles.item}>
-                                         <p className={styles.text}>Contacto</p>
-                                     </Link>
-                                     <Link to='/about' className={styles.item}>
-                                         <p className={styles.text}>Sobre Nosotros</p>
-                                     </Link>
+                        {
+                            isOpen ? (
+                                <Drawer
+                                    anchor='right'
+                                    open={true}
+                                    onClose={() => setIsOpen(!isOpen)}
+                                >
+                                    <div onClick={() => setIsOpen(!isOpen)} className={styles['btn-close']}>
+                                        <Icon name='x' />
+                                    </div>
+                                    <div className={styles['child-container-mobile']}>
+                                        <Link to='/payments' className={styles.item}>
+                                            <p className={styles.text}>Pagos</p>
+                                        </Link>
+                                        <Link to='/contact' className={styles.item}>
+                                            <p className={styles.text}>Contacto</p>
+                                        </Link>
+                                        <Link to='/about' className={styles.item}>
+                                            <p className={styles.text}>Sobre Nosotros</p>
+                                        </Link>
+                                    </div>
+                                </Drawer>
+                            ) : (
+                                <div 
+                                    onClick={() => setIsOpen(!isOpen)} 
+                                    className={styles.icon}
+                                >
+                                    <Icon name='menu' />
                                 </div>
-                            </Drawer>
-                        ) : (
-                            <div onClick={() => setIsOpen(!isOpen)}>
-                                <Icon name='menu' />
-                            </div>
-                        )
-                    }
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         </div>
